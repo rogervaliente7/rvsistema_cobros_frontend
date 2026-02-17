3375e6df5f875f1c72eb516358db4ae3ec11ab98
Marvin Eduardo Rodriguez Dominguez <rd18025@ues.edu.sv>
2026-02-16 21:30:48 -0600

    Cambios en módulo de gestiones y autenticación

--- src/app/authentication/authentication.component.ts
+++ src/app/authentication/authentication.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-authentication',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './authentication.component.html',
   styleUrl: './authentication.component.css'
 })
 export class AuthenticationComponent {
+  email = '';
+  password = '';
+  showPassword = false;
+  error = '';
 
+  togglePassword() {
+    this.showPassword = !this.showPassword;
+  }
+
+  login() {
+    if (!this.email || !this.password) {
+      this.error = 'Completa todos los campos';
+      return;
+    }
+
+    alert('Login OK (demo)');
+  }
 }
